52aa8b351db5a84cc636ff8eca0267db6b75cc16
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-19 17:26:13 +0200

    add icons for buttons and add icons styles

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,11 +1,32 @@
+import styled from 'styled-components';
 import Button from './Button';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSmile, faMeh, faFrown } from '@fortawesome/free-regular-svg-icons';
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  color: #666666;
+
+  button:hover & {
+    color: #fff;
+  }
+`;
+
+const getIcon = icon => <StyledIcon icon={icon} size="2xl" />;
+
 export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  const iconMap = {
+    Good: getIcon(faSmile),
+    Neutral: getIcon(faMeh),
+    Bad: getIcon(faFrown),
+  };
+
   return (
     <>
       {options.map(option => (
         <Button key={option} handler={onLeaveFeedback}>
-          {option}
+          <span>{iconMap[option]}</span>
+          <span>{option}</span>
         </Button>
       ))}
     </>
